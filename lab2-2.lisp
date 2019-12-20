52f50c0693d0b76d1ab1babdd1ce106d7945cff8
muratsalakhov <muratsalakhov@gmail.com>
2019-12-20 17:53:41 +0400

    Добавил 2 задание

---
+++ lab2-2.lisp
@@ -0,0 +1,5 @@
+(let ((in (open "D:/Workspace/Lisp lab2/file.txt" :if-does-not-exist nil)))
+      (when in
+            (loop for line = (read-line in nil)
+            while line do (format t "~a~%" line))
+      (close in)))
