00a480501c4b6defafca87741500e7a32aea17e3
muratsalakhov <muratsalakhov@gmail.com>
2019-12-21 12:41:33 +0400

    Исправил 2 задание

--- lab2-2.lisp
+++ lab2-2.lisp
@@ -1,9 +1,8 @@
-(let ((in (open "D:/Workspace/Lisp lab2/file.txt" :if-does-not-exist nil)))
+(defun textfile (file)
+	(let ((in (open file :if-does-not-exist nil)))
       (when in
             (loop for line = (read-line in nil)
             while line do (format t "~a~%" line))
-      (close in)))
+      (close in))))
 
-;; C:/roswell
-;; shift+ctrl+rightmouse открыть окно команл
-;; run ros.exe
+(textfile "D:/Workspace/Lisp lab2/file.txt")
